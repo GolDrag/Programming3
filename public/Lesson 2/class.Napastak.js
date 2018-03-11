54bfbbd8c8e0989d5d21e46903854c4582c57304
GolDrag <37072069+GolDrag@users.noreply.github.com>
2018-03-11 17:35:38 +0400

    Optimized class Napastak

--- public/Lesson 2/class.Napastak.js
+++ public/Lesson 2/class.Napastak.js
@@ -6,7 +6,7 @@ class Napastak {
     this.directions = [];
     this.index = 5;
   }
-  stanalNorKordinatner2() {
+  stanalNorKordinatner() {
     this.directions = [
       [this.x - 1, this.y - 1],
       [this.x, this.y - 1],
@@ -18,7 +18,7 @@ class Napastak {
       [this.x + 1, this.y + 1]
     ];
   }
-  stanalNorKordinatner() {
+  stanalNorKordinatnerSharj() {
     this.directions = [
       [this.x - 2, this.y - 2],
       [this.x, this.y - 2],
@@ -30,8 +30,8 @@ class Napastak {
       [this.x + 2, this.y + 2]
     ];
   }
-  yntrelVandakUt(ch) {
-    this.stanalNorKordinatner2()
+  yntrelVandak(ch) {
+    this.stanalNorKordinatner()
     var found = [];
     for (var i in this.directions) {
       var x = this.directions[i][0];
@@ -43,8 +43,8 @@ class Napastak {
       }
     }
     return found;
-  } yntrelVandak(ch) {
-    this.stanalNorKordinatner()
+  } yntrelVandakSharj(ch) {
+    this.stanalNorKordinatnerSharj()
     var found = [];
     for (var i in this.directions) {
       var x = this.directions[i][0];
@@ -59,8 +59,8 @@ class Napastak {
   }
 
   sharjvel() {
-    this.stanalNorKordinatner();
-    var datarkvandakner = this.yntrelVandak(0);
+    this.stanalNorKordinatnerSharj();
+    var datarkvandakner = this.yntrelVandakSharj(0);
     var norvandak = random(datarkvandakner);
     if (norvandak) {
       matrix[this.y][this.x] = 0;
@@ -71,8 +71,8 @@ class Napastak {
     }
   }
   eat() {
-    this.stanalNorKordinatner2();
-    var datarkvandakner = this.yntrelVandakUt(1);
+    this.stanalNorKordinatner();
+    var datarkvandakner = this.yntrelVandak(1);
     var norvandak = random(datarkvandakner);
 
     if (norvandak) {
@@ -110,4 +110,99 @@ class Napastak {
 
   }
 
+}*/
+class Napastak extends KendaniEak {
+  constructor(x, y, index) {
+    super(x, y, index)
+    this.tariq = 0
+  }
+  stanalNorKordinatner() {
+    this.directions = [
+      [this.x - 1, this.y - 1],
+      [this.x, this.y - 1],
+      [this.x + 1, this.y - 1],
+      [this.x - 1, this.y],
+      [this.x + 1, this.y],
+      [this.x - 1, this.y + 1],
+      [this.x, this.y + 1],
+      [this.x + 1, this.y + 1]
+    ];
+  }
+  stanalNorKordinatnerSharj() {
+    this.directions = [
+      [this.x - 2, this.y - 2],
+      [this.x, this.y - 2],
+      [this.x + 2, this.y - 2],
+      [this.x - 2, this.y],
+      [this.x + 2, this.y],
+      [this.x - 2, this.y + 2],
+      [this.x, this.y + 2],
+      [this.x + 2, this.y + 2]
+    ];
+  }
+  yntrelVandakSharj(ch) {
+    this.stanalNorKordinatnerSharj()
+    var found = [];
+    for (var i in this.directions) {
+      var x = this.directions[i][0];
+      var y = this.directions[i][1];
+      if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+        if (matrix[y][x] == ch) {
+          found.push(this.directions[i]);
+        }
+      }
+    }
+    return found;
+  }
+  sharjvel() {
+    this.stanalNorKordinatnerSharj();
+    var datarkvandakner = this.yntrelVandakSharj(0);
+    var norvandak = random(datarkvandakner);
+    if (norvandak) {
+      matrix[this.y][this.x] = 0;
+      matrix[norvandak[1]][norvandak[0]] = 5;
+      this.x = norvandak[0];
+      this.y = norvandak[1];
+      this.energy--;
+    }
+  }
+  eat() {
+    this.stanalNorKordinatner();
+    var datarkvandakner = this.yntrelVandak(1);
+    var norvandak = random(datarkvandakner);
+
+    if (norvandak) {
+
+      matrix[this.y][this.x] = 0;
+      matrix[norvandak[1]][norvandak[0]] = 5;
+      this.x = norvandak[0];
+      this.y = norvandak[1];
+      this.energy++;
+      for (var c in grassArr) {
+        if (grassArr[c].x == this.x && grassArr[c].y == this.y) {
+          grassArr.splice(c, 1);
+          break;
+
+        }
+      }
+    }
+    else {
+      this.sharjvel();
+    }
+  }
+
+  bazmanal() {
+    this.energy = 10;
+    var norVandak = random(this.yntrelVandak(0));
+    if (norVandak) {
+      var nornapastak = new Napastak(norVandak[0], norVandak[1]);
+      napastakner.push(nornapastak);
+      matrix[norVandak[1]][norVandak[0]] = 5;
+    }
+  }
+  mahanal(i) {
+    matrix[this.y][this.x] = 0;
+    napastakner.splice(i, 1);
+
+  }
 }
